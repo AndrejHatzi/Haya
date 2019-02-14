e9f4ee522dcadfe2041f8510809431f7e629ddb8
Andrej Hatzi <45495944+AndrejHatzi@users.noreply.github.com>
2019-02-14 16:17:23 +0000

    Patch #1
    Removed while loops, added file writing

---
+++ interpreter_deprecated.py
@@ -0,0 +1,337 @@
+from sly import Lexer
+from sly import Parser
+from fasterloop import faster_for_loop
+#remove var expr and add stmt
+class BasicLexer(Lexer):
+    tokens = { NAME, NUMBER, FLOAT, STRING, IF, ELSE, FOR, FASTERLOOP, WHILE, PRINT, FUN, TO, EQEQ, LEQ, DEL}
+    ignore = '\t '
+
+    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';', ':' }
+
+    # Define tokens
+    IF = r'if'
+    ELSE = r'else'
+    FOR = r'for'
+    FASTERLOOP = 'ffor'
+    WHILE = r'while'
+    FUN = r'function'
+    TO = r','
+    DEL = r'del'
+    PRINT = r'print'
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+    LEQ = r'<='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    #match floats
+    #Not working!!
+    @_(r'^[+-]?[0-9]*\.[0-9]+$"')
+    def FLOAT(self, t):
+        t.value = float(t.value)
+        return t
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self,t ):
+        self.lineno = t.value.count('\n')
+
+class BasicParser(Parser):
+    debugfile = 'parser.out'
+    tokens = BasicLexer.tokens
+
+    precedence = (
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS'),
+        )
+
+    def __init__(self):
+        self.env = { }
+    @_('')
+    def statement(self, p):
+        pass
+
+    @_('PRINT "(" statement ")"')
+    def statement(self, p):
+        return ('print_stmt_var', p.statement)
+
+    #@_('PRINT "(" expr ")"')
+    #def statement(self, p):
+        #return ('print_stmt_expr', p.expr)
+
+    #@_('PRINT "(" STRING ")"')
+    #def statement(self, p):
+        #return ('print_stmt_string', p.STRING)
+
+    #@_('PRINT "(" condition ")"')
+    #def statement(self, p):
+        #return ('print_stmt_condition', p.condition)
+
+    #for (x=5, 10): x
+    @_('FOR "(" var_assign TO expr ")" ":" statement')
+    def statement(self, p):
+        return ('for_loop', ('for_loop_setup', p.var_assign, p.expr), p.statement)
+
+    @_('FASTERLOOP "(" var_assign TO expr ")" ":" statement')
+    def statement(self, p):
+        return ('faster_for_loop', ('for_loop_setup', p.var_assign, p.expr), p.statement)
+
+    @_('WHILE "(" condition ")" ":" statement')
+    def statement(self, p):
+        return ('while_loop', ('while_loop_setup', p.condition), p.statement)
+
+    #if x==5: x
+    @_('IF "(" condition ")" ":" statement')
+    def statement(self, p):
+        return ('if_stmt', p.condition, ('branch', p.statement))
+
+    #if x==5: x else y
+    @_('IF "(" condition ")" ":" statement ELSE statement')
+    def statement(self, p):
+        return ('if_else_stmt', p.condition, ('branch', p.statement0, p.statement1))
+
+    #Deprecated
+    @_('FUN NAME "(" ")" ":" statement')
+    def statement(self, p):
+        return ('fun_def', p.NAME, p.statement)
+    #Deprecated
+    @_('NAME "(" ")"')
+    def statement(self, p):
+        return ('fun_call', p.NAME)
+
+    @_('expr EQEQ expr')
+    def condition(self, p):
+        return ('condition_eqeq', p.expr0, p.expr1)
+
+    @_('expr LEQ expr')
+    def condition(self, p):
+        return ('condition_leq', p.expr0, p.expr1)
+
+    @_('var_assign')
+    def statement(self, p):
+        return p.var_assign
+
+    @_('NAME "=" expr')
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.expr)
+
+    @_('NAME "=" STRING')
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.STRING)
+
+    @_('NAME "=" statement')
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.STRING)
+
+    @_('DEL NAME')
+    def statement(self, p):
+        return ('del_var', p.NAME)
+
+    @_('expr')
+    def statement(self, p):
+        return (p.expr)
+
+    @_('expr "+" expr')
+    def expr(self, p):
+        return ('add', p.expr0, p.expr1)
+
+    @_('expr "-" expr')
+    def expr(self, p):
+        return ('sub', p.expr0, p.expr1)
+
+    @_('expr "*" expr')
+    def expr(self, p):
+        return ('mul', p.expr0, p.expr1)
+
+    @_('expr "/" expr')
+    def expr(self, p):
+        return ('div', p.expr0, p.expr1)
+
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return p.expr
+
+    @_('NAME')
+    def expr(self, p):
+        return ('var', p.NAME)
+
+    @_('NUMBER')
+    def expr(self, p):
+        return ('num', p.NUMBER)
+
+    @_('FLOAT')
+    def expr(self, p):
+        return ('float', p.FLOAT)
+
+class BasicExecute:
+
+    def __init__(self, tree, env):
+        self.env = env
+        result = self.walkTree(tree)
+        print(env)
+        if result is not None and isinstance(result, int):
+            print(result)
+        if isinstance(result, str) and result[0] == '"':
+            print(result)
+
+    def walkTree(self, node):
+
+        if isinstance(node, int):
+            return node
+        if isinstance(node, str):
+            return node
+
+        if node is None:
+            return None
+
+        if node[0] == 'program':
+            if node[1] == None:
+                self.walkTree(node[2])
+            else:
+                self.walkTree(node[1])
+                self.walkTree(node[2])
+
+        if node[0] == 'num':
+            return node[1]
+
+        if node[0] == 'str':
+            return node[1]
+
+        if node[0] == 'if_stmt':
+            result = self.walkTree(node[1])
+            if result:
+                return self.walkTree(node[2][1])
+            return self.walkTree(node[2][2])
+
+        if node[0] == 'if_else_stmt':
+            result = self.walkTree(node[1])
+            if result:
+                return self.walkTree[2][1]
+
+        if node[0] == 'condition_eqeq':
+            return self.walkTree(node[1]) == self.walkTree(node[2])
+
+        if node[0] == 'condition_leq':
+            return self.walkTree(node[1]) <= self.walkTree(node[2])
+
+        if node[0] == 'fun_def':
+            self.env[node[1]] = node[2]
+
+        if node[0] == 'fun_call':
+            try:
+                return self.walkTree(self.env[node[1]])
+            except LookupError:
+                print("Undefined function '%s'" % node[1])
+                return 0
+
+        if node[0] == 'add':
+            return self.walkTree(node[1]) + self.walkTree(node[2])
+        elif node[0] == 'sub':
+            return self.walkTree(node[1]) - self.walkTree(node[2])
+        elif node[0] == 'mul':
+            return self.walkTree(node[1]) * self.walkTree(node[2])
+        elif node[0] == 'div':
+            return self.walkTree(node[1]) / self.walkTree(node[2])
+
+        if node[0] == 'var_assign':
+            self.env[node[1]] = self.walkTree(node[2])
+            return node[1]
+
+        if node[0] == 'del_var':
+            del self.env[node[1]]
+
+        if node[0] == 'var':
+            try:
+                return self.env[node[1]]
+            except LookupError:
+                print("Undefined variable '"+node[1]+"' found!")
+                return 0
+
+        if node[0] == 'for_loop':
+            if node[1][0] == 'for_loop_setup':
+                loop_setup = self.walkTree(node[1])
+
+                #searches for the var in the env and gets it's value
+                loop_count = self.env[loop_setup[0]]
+                loop_limit = loop_setup[1]
+
+                for i in range(loop_count+1, loop_limit+1):
+                    res = self.walkTree(node[2])
+                    #if res is not None: --> Deprecated for Print fun_call
+                        #print(res)
+                    self.env[loop_setup[0]] = i
+                del self.env[loop_setup[0]]
+
+        if node[0] == 'for_loop_setup':
+            return (self.walkTree(node[1]), self.walkTree(node[2]))
+
+        if node[0] == 'faster_for_loop':
+            if node[1][0] == 'for_loop_setup':
+                loop_setup = self.walkTree(node[1])
+                loop_count = self.env[loop_setup[0]]
+                loop_limit = loop_setup[1]
+                try:
+                    faster_for_loop(loop_count, loop_limit)
+                    del self.env[loop_setup[0]]
+                except:
+                    print('fast loops only accept integer values')
+
+        if node[0] == 'while_loop':
+            stmt = self.walkTree(node[1][1])
+            print(stmt)
+            while stmt:
+                res = self.walkTree(node[2])
+                if res:
+                    return self.walkTree(node[2][1])
+
+
+
+
+
+            #if node[1][0] == 'while_loop_setup':
+                #loop_setup = self.walkTree(node[1])
+                #print(loop_setup)
+
+        #It lacks var ref printing!
+        #if node[0] == 'print_stmt_string':
+            #print(node[1][1:-1])
+
+        if node[0] == 'print_stmt_var':
+            try:
+                print (self.env[node[1]])
+            except LookupError:
+                print("Undefined variable '"+node[1]+"' found!")
+                return 0
+
+        #if node[0] == 'print_stmt_expr':
+            #res = self.walkTree(node[1])
+            #print(res)
+
+        #if node[0] == 'print_stmt_condition':
+            #return self.walkTree(node[1])
+
+
+
+
+
+if __name__ == '__main__':
+    lexer = BasicLexer()
+    parser = BasicParser()
+    env = {}
+    while True:
+        try:
+            text = input('haya console > ')
+        except EOFError:
+            break
+        if text:
+            tree = parser.parse(lexer.tokenize(text))
+            BasicExecute(tree, env)
---
+++ stdio_lib/cmdargs.py
@@ -0,0 +1,4 @@
+import sys
+first_name = len(sys.argv[1])
+last_name = sys.argv[2]
+print ('{} {}'.format((first_name), last_name))
